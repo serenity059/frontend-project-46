898b8b5866d57a135c863778c43eaca8b4e9205c
Vladimir <nektoneizvestn007@gmail.com>
2024-12-17 17:43:40 +0300

    Настроен вывод справки

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse(process.argv);
