79aa0fbc0703847e4b4042faf80afddfaac748df
Vladimir <nektoneizvestn007@gmail.com>
2024-12-19 13:29:01 +0300

    Обновлена справка по gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
