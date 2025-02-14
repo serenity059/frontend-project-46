72cfaf6aab9c700980a7ec97c69d4f02ee3d1ddd
Vl <nektoneizvestn007@gmail.com>
2025-02-14 15:46:58 +0300

    14.02

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,16 +1,16 @@
 import _ from 'lodash';
 
 const getIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
+const getBracketIndent = (depth, spacesCount = 4) => ' '.repeat((depth - 1) * spacesCount);
+
 const formatValue = (value, depth) => {
   if (!_.isObject(value)) {
-    return value;
+    return String(value);;
   }
   const lines = Object.entries(value).map(
-    ([key, val]) => `${getIndent(depth + 1)} ${key}: ${formatValue(val, depth + 1)}`
+    ([key, val]) => `${getIndent(depth + 1)}  ${key}: ${formatValue(val, depth + 1)}`
   );
-  return `{
-${lines.join('\n')}
-${getIndent(depth)}  }`;
+  return `{\n${lines.join('\n')}\n${getBracketIndent(depth + 1)}}`;
 };
 
 const stylish = (diffTree, depth = 1) => {
@@ -25,9 +25,7 @@ const stylish = (diffTree, depth = 1) => {
       case 'changed':
         return `${getIndent(depth)}- ${key}: ${formatValue(oldValue, depth)}\n${getIndent(depth)}+ ${key}: ${formatValue(newValue, depth)}`;
       case 'nested':
-        return `${getIndent(depth)}  ${key}: {
-${stylish(children, depth + 1)}
-${getIndent(depth)}  }`;
+        return `${getIndent(depth)}  ${key}: {\n${stylish(children, depth + 1)}\n${getBracketIndent(depth + 1)}}`;
       default:
         throw new Error(`Incorrect type: ${type}`);
     }
@@ -35,6 +33,4 @@ ${getIndent(depth)}  }`;
   return lines.join('\n');
 };
 
-export default (diffTree) => `{
-${stylish(diffTree)}
-}`;
+export default (diffTree) => `{\n${stylish(diffTree)}\n}`;
